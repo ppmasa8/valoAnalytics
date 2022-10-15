978dbcad58e0e5becf4e57c79f92631f7a4937bf
ppmasa8 <kiyokiyopip56@gmail.com>
2022-10-16 08:18:25 +0900

    Added scrape script

---
+++ lib/scrape/game_updates.dart
@@ -0,0 +1,23 @@
+import 'package:html/parser.dart' show parse;
+import 'package:http/http.dart' as http;
+
+void main() async {
+  const url = 'https://www.oneesports.gg/valorant/';
+  final target = Uri.parse(url);
+
+  final response = await http.get(target);
+
+  // print(response.body);
+
+  if (response.statusCode != 200) {
+    print('ERROR: ${response.statusCode}');
+    return;
+  }
+
+  final document = parse(response.body);
+
+
+  final title = document.querySelectorAll('h2').map((v) => v.text).toList();
+
+  print(title);
+}
